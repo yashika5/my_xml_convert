f8eec4e1b0162b024b3eb127b8a0b16858942619
yashika5 <yashika9n@gmail.com>
2018-04-11 18:09:06 +0530

    added specs

--- lib/my_xml_convert.rb
+++ lib/my_xml_convert.rb
@@ -1,11 +1,17 @@
 require "my_xml_convert/version"
 require "my_xml_convert/ObjtoXML"
+require "my_xml_convert/key"
 
 module MyXmlConvert
 
   #translates a given object to an XML.
   def self.to_xml(obj, options = {})
   	ObjtoXML.to_xml(obj, options)
+  end
+
+  # Converts a given Hash key with options into an XML key.
+  def self.xml_tag(key, options = {})
+    MyXmlConvert::Key.create(key, options)
   end
 
 end
--- spec/my_xml_convert_spec.rb
+++ spec/my_xml_convert_spec.rb
@@ -1,45 +1,79 @@
 require 'my_xml_convert'
+require "my_xml_convert/ObjtoXML"
 
-describe MyXmlConvert.to_xml do
+describe "MyXmlConvert.to_xml" do
 
   it "array to xml" do
-    @book = [1,2,3,4,5]
-    expect(MyXmlConvert.to_xml(@book)).to eql("<Array><array>1</array><array>2</array><array>3</array><array>4</array><array>5</array></Array>")
+    book = [1,2,3,4,5]
+    expect(MyXmlConvert.to_xml(book)).to eql("<Array><array>1</array><array>2</array><array>3</array><array>4</array><array>5</array></Array>")
   end
 
+
   it "hash to xml" do
-    @book = {"1"=>1,"2"=>2,"3"=>3}
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<Hash>\n<book>\n   <1>1</1>\n   <2>2</2>\n   <3>3</3>\n<\\book>\n<\\Hash>\"\n")
+    book = {"1"=>1,"2"=>2,"3"=>3}
+    expect(MyXmlConvert.to_xml(book)).to eql("<Hash><1>1</1><2>2</2><3>3</3></Hash>")
   end
 
   it "fixnum to xml" do
-    @book = 3
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<Fixnum>\n<book>\n   <book>3</book>\n<\\book>\n<Fixnum>\"\n")
+    book = 3
+    expect(MyXmlConvert.to_xml(book)).to eql("<Fixnum>3</Fixnum>")
   end
  
   it "string to xml" do
-    @book = "a"
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<String>\n<book>\n   <book>a</book>\n<\\book>\n<String>\"\n")
+    book = "a"
+    expect(MyXmlConvert.to_xml(book)).to eql("<String>a</String>")
   end
 
   it "array inside array to xml" do
-    @book = [1,2,[1,2,3]]
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<Array>\n<book>\n   <book>1</book>\n   <book>2</book>\n   <book>\n      <book>1</book>\n      <book>2</book>\n      <book>3</book>\n   </book>\n<\\book>\n<\\Array>\"\n")
+    book = [1,2,[1,2,3]]
+    expect(MyXmlConvert.to_xml(book)).to eql("<Array><array>1</array><array>2</array><array><array>1</array><array>2</array><array>3</array></array></Array>")
   end
 
   it "hash inside array to xml" do
-    @book = [1,2,{"1"=>1}]
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<Array>\n<book>\n   <book>1</book>\n   <book>2</book>\n   <book>\n      <1>1</1>\n   </book>\n<\\book>\n<\\Array>\"\n")
+    book = [1,2,{"1"=>1}]
+    expect(MyXmlConvert.to_xml(book)).to eql("<Array><array>1</array><array>2</array><array><1>1</1></array></Array>")
   end
 
   it "array inside hash to xml" do
-    @book = {"1"=>[1,2,3],"2"=>2}
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<Hash>\n<book>\n   <1>\n      <1>1</1>\n      <1>2</1>\n      <1>3</1>\n   </1>\n   <2>2</2>\n<\\book>\n<\\Hash>\"\n")
+    book = {"1"=>[1,2,3],"2"=>2}
+    expect(MyXmlConvert.to_xml(book)).to eql("<Hash><1><1>1</1><1>2</1><1>3</1></1><2>2</2></Hash>")
   end
 
   it "hash inside hash to xml" do
-    @book = {"1"=>{"1"=>1,"2"=>2},"3"=>3}
-    expect(MyXmlConvert.to_xml(@book)).to eql("<?xml version=\"1.0\"encoding=\"UTF-8\"?>\n<Hash>\n<book>\n   <1>\n      <1>1</1>\n      <2>2</2>\n   </1>\n   <3>3</3>\n<\\book>\n<\\Hash>\"\n")
+    book = {"1"=>{"1"=>1,"2"=>2},"3"=>3}
+    expect(MyXmlConvert.to_xml(book)).to eql("<Hash><1><1>1</1><2>2</2></1><3>3</3></Hash>")
+  end
+
+  it "accepts a key_converter for the Hash keys" do
+    hash = { :user_name => "finn", :pass_word => "secret" }
+    xml = MyXmlConvert.to_xml(hash, {key_converter: :upcase})
+
+    expect(xml).to include("<USER_NAME>finn</USER_NAME>")
+    expect(xml).to include("<PASS_WORD>secret</PASS_WORD>")
+  end
+
+end
+
+describe "MyXmlConvert.xml_tag" do
+  
+  it "translates Symbols to lowerCamelCase by default" do
+    tag = MyXmlConvert.xml_tag(:user_name)
+    expect(tag).to eq("userName")
+  end
+
+  it "does not translate Strings" do
+    tag = MyXmlConvert.xml_tag("user_name")
+    expect(tag).to eq("user_name")
+  end
+
+  it "translates Symbols by a given key_converter" do
+    tag = MyXmlConvert.xml_tag(:user_name, :key_converter => :upcase)
+    expect(tag).to eq("USER_NAME")
+  end
+
+  it "does not translates Strings with a given key_converter" do
+    tag = MyXmlConvert.xml_tag("user_name", :key_converter => :upcase)
+    expect(tag).to eq("user_name")
   end
 
 end
