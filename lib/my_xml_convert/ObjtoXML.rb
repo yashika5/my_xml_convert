8a7e69ed1831afcb1539e8d300fd34057aa00e6b
yashika5 <yashika9n@gmail.com>
2018-04-11 15:43:59 +0530

    modified

--- lib/my_xml_convert/ObjtoXML.rb
+++ lib/my_xml_convert/ObjtoXML.rb
@@ -16,10 +16,27 @@ module MyXmlConvert
 				xmlstring+= "<?xml version=\"1.0\"encoding=\"UTF-8\"?>" if options[:header]
 				xmlstring+= "<" + options[:name] + ">" if options[:name]				
 
-				if obj.instance_of?(Hash)
+				if options[:type] == "Hash"
 					xmlstring+= "<Hash>"
 					xmlstring+= MyXmlConvert::Hash.hash_to_xml(obj, options)
 					xmlstring+= "</Hash>"
+				elsif obj.class == Array
+					xmlstring+= "<Array>"
+					xmlstring+= MyXmlConvert::Array.array_to_xml(obj, options)
+					xmlstring+= "</Array>"
+				elsif obj.class == String
+					xmlstring+= "<String>"
+					xmlstring+= MyXmlConvert::String.string_to_xml(obj,options)
+					xmlstring+= "</String>"
+				elsif obj.class == Fixnum
+					xmlstring+= "<Fixnum>"
+					xmlstring+= MyXmlConvert::Fixnum.fixnum_to_xml(obj, options)
+					xmlstring+= "</Fixnum>"
+				else
+					xmlstring+= "<Object>"
+                    value = MyXmlConvert::Value.create(obj,options)
+					xmlstring+= value.to_s
+                    xmlstring+= "</Object>"
 				end
 
 				xmlstring+= "</" + options[:name] + ">" if options[:name]
