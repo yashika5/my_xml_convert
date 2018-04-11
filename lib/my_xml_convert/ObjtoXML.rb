1215ef802362cc9bb95387d099db534afb13bb61
yashika5 <yashika9n@gmail.com>
2018-04-11 15:30:37 +0530

    modified

--- lib/my_xml_convert/ObjtoXML.rb
+++ lib/my_xml_convert/ObjtoXML.rb
@@ -12,6 +12,7 @@ module MyXmlConvert
 
 			def self.to_xml(obj, options = {} )
 
+				puts obj.class
 				xmlstring = "" 
 				xmlstring+= "<?xml version=\"1.0\"encoding=\"UTF-8\"?>" if options[:header]
 				xmlstring+= "<" + options[:name] + ">" if options[:name]				
